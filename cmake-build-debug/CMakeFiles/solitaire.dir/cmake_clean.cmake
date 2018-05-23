a471900ed3dafd36e2a5a92f190ae8644b1c3d62
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2018-05-23 11:50:41 -0300

    removing ignored files

---
+++ cmake-build-debug/CMakeFiles/solitaire.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solitaire.dir/solitaire.cpp.o"
+  "solitaire.pdb"
+  "solitaire"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solitaire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
