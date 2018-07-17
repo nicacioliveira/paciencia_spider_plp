994bb404af976a70b674673bb27327fec6b90144
KelvinCL <39067792+KelvinCL@users.noreply.github.com>
2018-07-17 16:02:22 -0300

    Update solitaire.pl

--- solitaire.pl
+++ solitaire.pl
@@ -32,14 +32,14 @@ nl,writeln(" _______  _______  ___      ___   _______  _______  ___   ______    
    
 helpGame :- 
 nl, writeln("  |---------------------------------HELP--------------------------------|"),
-    writeln("  |Start:            start                                              |"),
-    writeln("  |Quit Game:        quit                                               |"),
-    writeln("  |Reset Game:       reset                                              |"),
-    writeln("  |Help:             help                                               |"),
-    writeln("  |Hint:             hint                                               |"),
-    writeln("  |print piles:      print                                              |"),
-    writeln("  |deal:             deal                                               |"),
-    writeln("  |Completed Suits:  suits                                              |"),
+    writeln("  |Start:            start (1)                                             |"),
+    writeln("  |Reset Game:       reset (2)                                             |"),
+    writeln("  |Help:             help (3)                                              |"),
+    writeln("  |Hint:             hint (4)                                              |"),
+    writeln("  |print piles:      print (5)                                             |"),
+    writeln("  |deal:             deal (6)                                              |"),
+    writeln("  |Completed Suits:  suits (7)                                             |"),
+    writeln("  |Quit Game:        quit (8)                                              |"),
     writeln("  |_____________________________________________________________________|"),
     writeln("  |Move cards:    move (press Enter)                                    |"),
     writeln("  |               <card value> <output pile number> <input pile number> |"),
@@ -84,6 +84,24 @@ nl, writeln("                    ____             _ _                        "),
    
 		
 
+%nao testado		
+userInput :-
+    read_line_to_codes(user_input, X3),
+    string_to_atom(X3,X2),
+    atom_number(X2,X),
+    processInput(X).
+ 
+% os metodos start, reset, help, hint, print, deal, suits, exit e exception nao foram implementados    
+processInput(X) :-   
+	X ==:== 1 -> start();
+	X ==:== 2 -> reset();
+	X ==:== 3 -> help();
+	X ==:== 4 -> hint();
+	X ==:== 5 -> print();
+	X ==:== 6 -> deal();
+	X ==:== 7 -> suits();
+        X ==:== 8 -> exit();
+        X > 8 -> exception().
 
 
 
